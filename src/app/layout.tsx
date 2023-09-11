ab2befde68ccbb3f38e825ee7251520760f49ec0
Muhammed Oguz <muhammed0748@outlook.com>
2023-09-11 05:34:36 +0300

    Update vscode settings and add theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,26 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { ThemeProvider } from "@/components/theme/ThemeProvider";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ReactNode } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Rota Finder',
-  description: 'Find the best rota for your flight',
+  title: "Rota Finder",
+  description: "Find the best rota for your flight",
+};
+
+interface RootLayoutProps {
+  children: ReactNode;
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <ThemeProvider>
+        <body className={inter.className}>{children}</body>
+      </ThemeProvider>
     </html>
-  )
+  );
 }
