655efaa3e46756b272119f1970c4dc43e1bded69
Muhammed Oguz <muhammed0748@outlook.com>
2023-09-13 02:42:17 +0300

    Create typograpghy and background image and add caption

--- src/components/button/ThemeSwitchButton.tsx
+++ src/components/button/ThemeSwitchButton.tsx
@@ -3,13 +3,18 @@
 import { Button, ButtonProps } from "@/components/ui/button";
 import { MoonIcon, SunIcon } from "lucide-react";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 type ThemeSwitchButtonProps = ButtonProps;
 
 const ThemeSwitchButton = ({ ...props }: ThemeSwitchButtonProps) => {
   const { theme, setTheme } = useTheme();
+  const [isLightMode, setIsLightMode] = useState(theme === "light");
 
-  const isLightMode = theme === "light";
+  useEffect(() => {
+    setIsLightMode(theme === "light");
+  }, [theme]);
+
   return (
     <Button
       variant="secondary"
